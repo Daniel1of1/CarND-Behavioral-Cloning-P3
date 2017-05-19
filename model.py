30790c76ca09efe896633bb95917e83d1d609757
Daniel Haight <daniel@haight.io>
2017-05-19 22:05:20 +0100

    add comments to model.py

--- model.py
+++ model.py
@@ -3,13 +3,14 @@ import csv
 import pandas as pd
 import numpy as np
 
+# Parse all csv rows into `lines` list
 lines = []
 with open('./driving_data/driving_log.csv') as csvfile:
 	reader = csv.reader(csvfile)
 	for line in reader:
 		lines.append(line)
 
-
+# Create lists of images and corresponding mesurements
 images = []
 measurements = []
 for line in lines:
@@ -24,7 +25,7 @@ for line in lines:
 	measurements.append(measurement+0.2)
 	measurements.append(measurement-0.2)
 
-
+# convert to np arrays for keras
 x_train = np.asarray(images)
 y_train = np.asarray(measurements)
 
@@ -34,8 +35,11 @@ from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 from keras.optimizers import Adam
 
+# Build Model 
 model = Sequential()
+# normalize image
 model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
+# crop parts of the image with little road information
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
@@ -50,6 +54,7 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 
+# train with a validation split
 model.fit(x_train, y_train, validation_split=0.2,shuffle=True, nb_epoch=3)
 
 model.save('model.h5')
